ab78681cee28027e6f16505a01ce2893d52f78d4
shavonnecobb2 <shavonnedenisecobb@gmail.com>
2020-08-13 16:20:46 -0500

    first commit

---
+++ src/main/java/com/shavonne/iridescence7/Iridescence7Application.java
@@ -0,0 +1,13 @@
+package com.shavonne.iridescence7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iridescence7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Iridescence7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/shavonne/iridescence7/Iridescence7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shavonne.iridescence7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Iridescence7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
